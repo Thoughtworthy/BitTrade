0a05825d0504e646fde137ce7b35f4c70b15628a
Torosyan David <davit.torosyan.0214@gmail.com>
2023-08-01 21:23:56 +0400

    Add project files.

---
+++ BitTrade/Scripts/my-js/my-js.js
@@ -0,0 +1,27 @@
+﻿var navOpen = false;
+
+function toggleNav() {
+	if (navOpen) {
+		closeNav();
+	} else {
+		openNav();
+	}
+	navOpen = !navOpen;
+}
+
+//function openNav() {
+//	document.getElementById("myNav").style.width = "250px";
+//}
+
+//function closeNav() {
+//	document.getElementById("myNav").style.width = "0";
+//}
+/* Toggle sidebar
+	 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+function openNav() {
+	document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mySidepanel").style.width = "0";
+}
